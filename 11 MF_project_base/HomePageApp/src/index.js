f60152221a54d087dbd01fe44713500ef7fad89b
Divya Sharma <dsrsaknp@gmail.com>
2022-12-19 09:12:34 +0530

    Fourth Commit

--- 11 MF_project_base/HomePageApp/src/index.js
+++ 11 MF_project_base/HomePageApp/src/index.js
@@ -1,6 +1,1 @@
-import '@babel/polyfill';   // for transpiling async await code to js
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("app"));
+import("./bootstrap");
